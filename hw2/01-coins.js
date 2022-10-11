1f279f4f511c184e377cd8581e92e32a8baa7537
CristianTS599 <cristiant.salazar@gmail.com>
2022-10-11 07:40:21 -0700

    Contains changes for initial coins.js version file

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -2,6 +2,66 @@
 
 const calculateChange = (input) => {
   // Add your code here
+
+  //if (typeof input != typeof number) {
+  //console.log("Error: Please enter a number.");
+  //}
+  if (input > 10 || input < 0) {
+    return "Error: Value must be between 0 and 10";
+  }
+  if (input === 0) {
+    return "No Change.";
+  }
+
+  var dollar = 0;
+  var quarter = 0;
+  var dime = 0;
+  var nickel = 0;
+  var penny = 0;
+
+  var out = "";
+
+  while (input > 0) {
+    if (input > 1) {
+      dollar += 1;
+      input -= 1;
+    } else if (input > 0.25) {
+      quarter += 1;
+      input -= 0.25;
+    } else if (input > 0.1) {
+      dime += 1;
+      input -= 0.1;
+    } else if (input > 0.05) {
+      nickel += 1;
+      input -= 0.05;
+    } else {
+      if (input < 0.02) {
+        penny += 1;
+        input = 0;
+      } else {
+        penny += 1;
+        input -= 0.01;
+      }
+    }
+  }
+
+  if (dollar !== 0) {
+    out += dollar + ` dollar${dollar === 1 ? `` : `s`}, `;
+  }
+  if (quarter !== 0) {
+    out += quarter + ` quarter${quarter === 1 ? `` : `s`}, `;
+  }
+  if (dime !== 0) {
+    out += dime + ` dime${dime === 1 ? `` : `s`}, `;
+  }
+  if (nickel !== 0) {
+    out += nickel + ` nickel${nickel === 1 ? `` : `s`}, `;
+  }
+  if (penny !== 0) {
+    out += penny + ` penn${penny === 1 ? `y` : `ies`}`;
+  }
+
+  return out;
 };
 
 // Sample Test Cases
