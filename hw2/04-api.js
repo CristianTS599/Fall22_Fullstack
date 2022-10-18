3344abc1a3cee47f4394bca384b59f20b27fe753
CristianTS599 <cristiant.salazar@gmail.com>
2022-10-18 13:43:15 -0700

    implements progress for the api part

--- hw2/04-api.js
+++ hw2/04-api.js
@@ -1,7 +1,27 @@
 /** Exercise 04 - API **/
 
-const url = 'https://restcountries.com/v3.1/all';
+const url = "https://restcountries.com/v3.1/all";
 
 // Add your code here
+
+function getData(getUrl) {
+  fetch(getUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      let count = 1;
+      data.forEach((country) => {
+        let item = `${count}. ${country.name.common} - ${country.population}`;
+        document.getElementById("results").append(`<li>${item}</li>`);
+        ++count;
+        console.log(
+          `Country Name ${country.name.common} - Population ${country.population}`
+        );
+      });
+    })
+    .catch((error) => console.log(error));
+}
 
 getData(url);
